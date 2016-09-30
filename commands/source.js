6de53a38e6548b97d04e4f2469b77bf2469e2071
Rxswyers <rxswyers@gmail.com>
2016-09-30 19:14:52 -0400

    Created package.json and source command

---
+++ commands/source.js
@@ -0,0 +1,18 @@
+//gives a link to the source code on github
+module.exports = function command(bot, info)
+{
+    "use strict";
+    const pack = require('./package.json');
+    return {
+        alias: ['s'],
+        description: "(Gets the bot's source)",
+        permissions: "public",
+        action: function(details)
+        {
+            bot.sendMessage({
+                to: details.channelID,
+                message: pack.homepage
+            })
+        }
+    };
+};
