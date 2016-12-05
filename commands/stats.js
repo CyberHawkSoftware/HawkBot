8d3dfd40ac36b45cadd4492d6239c3b3fa236bbd
Ruben <rswyers@cyberhawk.co>
2016-12-05 10:06:19 -0500

    Updated stats command to an embed

--- commands/stats.js
+++ commands/stats.js
@@ -5,14 +5,53 @@ module.exports = function command(bot, info)
     const pack = require('../package.json');
     return {
         alias: ['s'],
-        description: "(Returns how many servers this bot is currently a part of)",
-        permissions: "private",
+        description: "(Returns some bot stats)",
+        permissions: "public",
         action: function(details)
         {
-            bot.sendMessage({
+                bot.sendMessage({
                 to: details.channelID,
-                message: "```HawkBot is now on " + Object.keys(bot.servers).length + " servers!```"
-            })
+                embed: {
+                    title: 'HawkBot Stats',
+                    description: '',
+                    footer: {
+                        text: "Created by CyberRonin#5517"
+                    },
+                    thumbnail: {
+                        url: "https://cdn.discordapp.com/avatars/193403332046487552/cc2379129ee5450d617a1170a468e4f9.jpg"
+                    },
+                    fields:[
+                    {
+                        name: "Servers",
+                        value: Object.keys(bot.servers).length,
+                        inline: true
+                    },
+                    {
+                        name: "Channels",
+                        value: Object.keys(bot.channels).length,
+                        inline: true
+                    },
+                    {
+                        name: "Users",
+                        value: Object.keys(bot.users).length,
+                        inline: true
+                    },
+                    {
+                        name: "Commands",
+                        value: Object.keys(info.commands).length,
+                        inline: true
+                    },
+                    {
+                        name: "About",
+                        value: "Primary purpose is to serve as a Jisho lookup, but there are fun commands as well. The beta and master branches have a DJ implementation."
+                    },
+                    {
+                        name: "Github",
+                        value: "[Public Branch - no audio](https://github.com/CyberHawkSoftware/HawkBot/tree/public)\n[Beta Branch - Most up to date w/audio](https://github.com/CyberHawkSoftware/HawkBot/tree/beta)\n[Master Branch w/audio](https://github.com/CyberHawkSoftware/HawkBot/tree/master)",
+                        inline: true
+                    }]
+                }
+            });
         }
     };
 };
