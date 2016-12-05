b3021f1e88a918edf480c650f9d8165111315c60
Ruben <rswyers@cyberhawk.co>
2016-12-05 11:33:34 -0500

    Added eval

---
+++ commands/eval.js
@@ -0,0 +1,33 @@
+//evaluates js code
+module.exports = function command(bot, info)
+{
+    "use strict";
+    return {
+        //the alias can be set by putting what you'd like in that array
+        alias: ['ev'],
+        //description that shows up in the help menu
+        description: "(Evaluates JS code)",
+        //set the permissions [public, elevated, mod, private]
+        permissions: "private",
+        action: function(details)
+        {
+          if(details.input === "") {return;}
+          else
+          {
+            try{
+              bot.sendMessage({
+                to: details.channelID,
+                message: eval(details.input)
+              });
+            }
+            catch(err)
+            {
+              bot.sendMessage({
+                to: details.channelID,
+                message: err
+              });
+            }
+          }
+        }
+    };
+};
