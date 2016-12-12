3574b97a2b1e11755c569c296c3d426445b978bf
Ruben <rswyers@cyberhawk.co>
2016-12-12 12:38:25 -0500

    Added random picture generator, good for prompts

---
+++ commands/prompt.js
@@ -0,0 +1,47 @@
+//template command
+//when calling the commands, loader.js will create the command based on what the
+//name of the file is.
+module.exports = function command(bot, info)
+{
+    "use strict";
+    return {
+        //the alias can be set by putting what you'd like in that array
+        alias: ['p'],
+        //description that shows up in the help menu
+        description: "(Gets random pictures)",
+        //set the permissions [public, elevated, mod, private]
+        permissions: "public",
+        action: function(details)
+        {
+          const sendImage = function()
+          {
+            const request = require("request");
+            const url = "http://www.splashbase.co/api/v1/images/random";
+            request({
+              url: url,
+              json: true
+            }, function (error, response, body)
+            {
+              if(!error && response.statusCode === 200)
+              {
+                //let catResponse = JSON.parse(body);
+                bot.sendMessage({
+                  to: details.channelID,
+                  embed:{
+                    title: '',
+                    description: '',
+                    image:{
+                      url: body.url
+                    }
+                  }
+                });
+              }
+            });
+          }
+          if(details.input === "")
+          {
+            sendImage();
+          }
+        }
+    };
+};
